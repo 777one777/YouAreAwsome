68a6bc8d2ae7444d62f7207d2bff775d5b31ce2d
777one777 <jcuevavasquez2025@gmail.com>
2025-02-27 14:24:36 -0800

    February 25, 2025 Sounds and For Loops

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var lastMessageNumber = -1 // lastMessageNumber will never be -1
     @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var  audioPlayer : AVAudioPlayer!
+    let numberOfImages = 10 // images labeled image0 - image9
+    let numberOfSounds = 6 // sounds labeled sound0 - sound5
     
     var body: some View {
         
@@ -24,6 +29,8 @@ struct ContentView: View {
                 .minimumScaleFactor(0.5)
                 .frame(height: 100)
                 .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Spacer()
             
             Image(imageName)
                 .resizable()
@@ -42,13 +49,6 @@ struct ContentView: View {
                                 "Fabulous? That's You!",
                                 "You make me Smile!"]
                 
-                // generate a random messageNumber to use an index
-                // if messageNumber == lastMessageNumber {
-                //  keep generating a new random messageNumber
-                //  until you get a messageNumber != lastMessageNumber
-                // set messageString to messages[messageNumber]
-                // update the LastMessageNumber with messageNumber
-                
                 var messageNumber = Int.random(in: 1...messages.count-1)
                 while messageNumber == lastMessageNumber {
                     messageNumber = Int.random(in: 0...messages.count-1)
@@ -56,13 +56,30 @@ struct ContentView: View {
                 message = messages[messageNumber]
                 lastMessageNumber = messageNumber
                 
-               // imageName = "image\(Int.random(in: 0...9))"
-                var imageNumber = Int.random(in: 0...9)
-                while imageNumber == lastImageNumber {
-                    imageNumber = Int.random(in: 0...9)
-                }
+                var imageNumber : Int
+                repeat {
+                    imageNumber = Int.random(in: 0...numberOfImages-1)
+                } while imageNumber == lastImageNumber
                 imageName = "image\(imageNumber)"
                 lastImageNumber = imageNumber
+                
+                var soundNumber: Int
+                repeat{
+                    soundNumber = Int.random(in: 0...numberOfSounds-1)
+                } while soundNumber == lastSoundNumber
+                lastSoundNumber = soundNumber
+                let soundName = "sound\(soundNumber)"
+                
+                guard let soundFile = NSDataAsset( name: soundName) else {
+                    print("😡 Could not read file named\(soundName)")
+                    return
+                }
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                } catch{
+                    print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+                }
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)        }
